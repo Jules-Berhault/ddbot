387dc8ce352376a1dd6a433b9f106db369d96eef
Teusner <quentin.brateau@ensta-bretagne.org>
2020-01-31 16:11:25 +0100

    Projection Node

---
+++ src/projection.cpp
@@ -0,0 +1,17 @@
+#include "ros/ros.h"
+#include <WGS84toCartesian.hpp>
+
+int main(int argc, char **argv) {
+
+    // Setting the loop rate
+    double h = 1.0/25.0;
+    ros::Rate loop_rate(25);
+
+    
+
+    // Loop 
+    while (ros::ok()) {
+
+    }
+    return 0;
+}
