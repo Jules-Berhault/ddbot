8d62717cbe6a770ba9ed9d8e899e21239b8167a0
gwendalp <gwendal.priser@ensta-bretagne.org>
2020-02-01 23:36:22 +0100

    lissajoux

--- doc/simulation.py
+++ doc/simulation.py
@@ -14,46 +14,38 @@ def dynamique(X, u):
 
 def control(x, w, dw):
     xf = x.flatten()    
-
     px, py, theta, v = xf
     ct = np.cos(theta)
     st = np.sin(theta)
     A = np.array([[ct - v*st, ct + v*st], [st + v*ct, st -v*ct]])
     B = np.array([[-abs(v)*v*ct],[-abs(v)*v*st]])
-    
     y = np.array([[px], [py]])
     dy = np.array([[v*ct], [v*st]])
-    
-    v = (w - y) + 2*(dw - dy)
+    v = 2*(w - y) + 2*(dw - dy)
     u =  np.linalg.solve(A, (v - B))
     return u
 
 
 def set_point(t):
-    omega = 0.5
-    w = R*array([[cos(omega*t)], [sin(omega*t)]])
-    dw = R*array([[-omega*sin(omega*t)], [omega*cos(omega*t)]]) 
+    omega = 0.1
+    phi = 0
+    w = array([[a * cos(omega*t+phi), a/2 * sin(2*omega*t+phi)]]).T
+    dw = array([[- a *omega*sin(omega*t+phi), a * omega*cos(omega*t+phi)]]).T
     return w, dw 
 
 
 if __name__ == "__main__" :
     X = array([[0.1, 0.1, 0.1, 0.1]]).T
     u = array([[0, 0]]).T
-    
-    R = 5
+    a = 18
     theta = linspace(0,  2*pi, 100)
-    cx = R*cos(theta)
-    cy = R*sin(theta)
-    
-    
+    cx = a * cos(theta)
+    cy = a/2 * sin(2*theta)
     dt = 0.05
-
-    ax = init_figure(-10, 10, -10, 10)
-
-    for t in arange(0, 10, dt):
+    ax = init_figure(-20, 20, -20, 20)
+    for t in arange(0, 300, dt):
         clear(ax)
         w, dw = set_point(t)
-    
         u = control(X, w, dw)
         X = X + dt * dynamique(X, u)
         plt.plot(cx, cy, color="crimson", alpha=0.4, linewidth=2)
