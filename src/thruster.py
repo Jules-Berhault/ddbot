8b6960d25f4ef387ad25a5a68da4909a8e43d1bd
Teusner <quentin.brateau@ensta-bretagne.org>
2020-02-04 21:49:42 +0100

    Cleaning thruster.py

--- src/thruster.py
+++ src/thruster.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-# license removed for brevity
 import rospy
 from std_msgs.msg import Float64
 from arduino_driver import *
+
 """ Variable du Noeud """
 global cmd_u1
 global cmd_u2
@@ -11,8 +11,6 @@ global cmd_max
 cmd_max=255
 cmd_u1=40
 cmd_u2=0
-
-
 
 def callback_u1(data):
     global cmd_u1
@@ -35,10 +33,6 @@ arduino=init_arduino_line()
 
 rate = rospy.Rate(25) # 25hz
 
-
 while not rospy.is_shutdown():
-
     send_arduino_cmd_motor(arduino,clip_cmd(cmd_u1,cmd_max),clip_cmd(cmd_u2,cmd_max))
-    
     rate.sleep()
-
